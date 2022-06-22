cf31def2da901de156b52ca71b6039f7a562ecdf
unknown <ovic391@gmail.com>
2022-06-22 20:57:55 +0200

    adding fetch

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -1,21 +1,49 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-const First = () => {
-    const {
-        query: { blogId },
-    } = useRouter()
+import Link from 'next/link';
 
-    return (
-        <div>
-            <Link href='/blog'>
-                <a>got to blogs</a>
-            </Link>
-            <h1>
-                This is Blog Num
-                {blogId}
-            </h1>
-        </div>
-    )
+const First = ({ post }) => {
+  return (
+    <div>
+      {post && (
+        <>
+          <Link href='/blog'>
+            <a>got to blogs</a>
+          </Link>
+          <h1>
+            This is Blog Num
+            {post.id}
+          </h1>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default First;
+
+export async function getStaticProps(context) {
+  const {
+    params: { blogId },
+  } = context;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${blogId}`
+  );
+  const data = await res.json();
+  return {
+    props: { post: data },
+  };
 }
 
-export default First
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  const paths = data.map((el) => {
+    return { params: { blogId: `${el.id}` } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,30 +1,32 @@
-import Link from 'next/link'
-const index = () => {
-    return (
-        <div>
-            <Link href='/'>
-                <a>return Home</a>
-            </Link>
-            <h1>This is Blog Page</h1>
-            <ul>
-                <li>
-                    <Link href='/blog/1'>
-                        <a>Blog 1</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href='/blog/2'>
-                        <a>Blog 2</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href='/blog/3'>
-                        <a>Blog 3</a>
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+import Link from 'next/link';
+const index = ({ posts }) => {
+  return (
+    <div>
+      <Link href='/'>
+        <a>return Home</a>
+      </Link>
+      <h1>This is Blog Page</h1>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li>
+              <Link href={`/blog/${post.id}`}>
+                <a>Blog {post.id}</a>
+              </Link>
+            </li>
+          );
+        })}{' '}
+      </ul>
+    </div>
+  );
+};
 
-export default index
+export default index;
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return {
+    props: { posts: data },
+  };
+}
