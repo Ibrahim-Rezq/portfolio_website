2b6f07111dc7c5d3d164d4663f0b35fc20dfeb29
unknown <ovic391@gmail.com>
2022-11-27 16:40:04 +0200

    env update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
+const result = require("dotenv").config()
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SERVE_URL: "http://localhost:3000/api/",
-    // "https://my-json-server.typicode.com/Ibrahim-Rezq/portfolio_website/projects/"
-  },
+  env: result.parsed,
   images: {
     domains: ["picsum.photos", "loremipsum.io"],
   },
